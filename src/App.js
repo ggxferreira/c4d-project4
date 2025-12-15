8fa1c1fcf82cd55148c9ac7d339dc335500841d5
Giulia <giulia@Giulias-MacBook-Pro.local>
2025-12-15 14:13:04 -0600

    adding all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my first react app</h1>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App;
